9bf7837497a88d28a72e02cf3c5b547039f2e589
Petr Dvorak <petr@inmite.eu>
2017-09-25 01:54:52 +0200

    Implement basic reader and processor for device push tokens Bump library versions

---
+++ powerauth-push-server/src/main/java/io/getlime/push/service/batch/UserDeviceItemProcessor.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2016 Lime - HighTech Solutions s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.getlime.push.service.batch;
+
+import io.getlime.push.repository.model.aggregate.UserDevice;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author Petr Dvorak, petr@lime-company.eu
+ */
+@Component
+@StepScope
+public class UserDeviceItemProcessor implements ItemProcessor<UserDevice, UserDevice> {
+
+    private ProcessingItemStore<UserDevice> itemStore = new HashSetProcessingItemStore<>();
+
+    @Override
+    public UserDevice process(UserDevice userDevice) throws Exception {
+        if (itemStore.exists(userDevice)) {
+            return null;
+        }
+        return itemStore.put(userDevice);
+    }
+
+}
