da9a4fdf8a1837a4f445ed00e6582a08ecc5060f
Roman Strobl <roman.strobl@wultra.com>
2022-11-14 11:46:07 +0100

    Add @Modifying annotation

--- powerauth-push-server/src/main/java/io/getlime/push/repository/InboxRepository.java
+++ powerauth-push-server/src/main/java/io/getlime/push/repository/InboxRepository.java
@@ -18,6 +18,7 @@ package io.getlime.push.repository;
 
 import io.getlime.push.repository.model.InboxMessageEntity;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.stereotype.Repository;
@@ -75,6 +76,7 @@ public interface InboxRepository extends PagingAndSortingRepository<InboxMessage
      * @param userId User ID.
      */
     @Query("UPDATE InboxMessageEntity i SET i.read = true WHERE i.userId = :userId AND i.read = false")
+    @Modifying
     int markAllAsRead(String userId);
 
 }
