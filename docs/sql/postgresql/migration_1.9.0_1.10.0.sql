bcd6b7072a296daa60827a0f173016352af6ae73
Lubos Racansky <lubos.racansky@gmail.com>
2025-07-16 12:48:53 +0200

    Fix #997: Migration is skipped

--- docs/sql/postgresql/migration_1.9.0_1.10.0.sql
+++ docs/sql/postgresql/migration_1.9.0_1.10.0.sql
@@ -16,11 +16,11 @@ ALTER TABLE push_app_credentials ADD fcm_private_key BYTEA;
 
 ALTER TABLE push_app_credentials ADD fcm_project_id VARCHAR(255);
 
--- Changeset powerauth-push-server/1.10.x/20241029-migrate-ios-to-apns.xml::3::Roman Strobl
+-- Changeset powerauth-push-server/1.10.x/20241029-migrate-ios-to-apns.xml::5::Lubos Racansky
 -- Migrate existing ios_* columns to apns_* columns
 UPDATE push_app_credentials SET apns_bundle = ios_bundle, apns_environment = ios_environment, apns_key_id = ios_key_id, apns_private_key = ios_private_key, apns_team_id = ios_team_id;
 
--- Changeset powerauth-push-server/1.10.x/20241029-migrate-android-to-fcm.xml::4::Roman Strobl
+-- Changeset powerauth-push-server/1.10.x/20241029-migrate-android-to-fcm.xml::6::Lubos Racansky
 -- Migrate existing android_* columns to fcm_* columns
 UPDATE push_app_credentials SET fcm_private_key = android_private_key, fcm_project_id = android_project_id;
 
