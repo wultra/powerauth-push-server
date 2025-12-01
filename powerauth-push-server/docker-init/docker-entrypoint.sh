b5f2647b7f8d9674e30ebd765f41d4fce6ecd91a
Filip Bursik <filip.bursik@wultra.com>
2025-12-01 18:18:02 +0200

    Refactor configuration for push server to use environment variables and add Docker CI

---
+++ powerauth-push-server/docker-init/docker-entrypoint.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -euo pipefail
+KEEP_RUNNING=${KEEP_RUNNING:-false}
+KEEP_RUNNING_PORT=${KEEP_RUNNING_PORT:-666}
+
+liquibase --headless=true --log-level=INFO \
+    --changeLogFile="changelog/powerauth-push-server/db.changelog-module.xml" \
+    --username="${POWERAUTH_PUSH_SERVER_DATASOURCE_USERNAME:-}" \
+    --password="${POWERAUTH_PUSH_SERVER_DATASOURCE_PASSWORD:-}" \
+    --url="${POWERAUTH_PUSH_SERVER_DATASOURCE_URL}" \
+    update
+
+if [[ -n "${KEEP_RUNNING}" && "${KEEP_RUNNING}" == "true" ]]; then
+    echo "Container kept alive via KEEP_RUNNING and KEEP_RUNNING_PORT flag"
+    RESPONSE="HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: 15\r\n\r\nHello from nc!\n"
+
+    while true; do
+      echo -e "$RESPONSE" | nc -l -p "$KEEP_RUNNING_PORT"
+    done
+fi
