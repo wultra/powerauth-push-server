a98ca3229c79b9e1621f7fe7da33fb03c70eb428
Roman Strobl <roman.strobl@wultra.com>
2026-05-14 10:51:46 +0200

    Update sequence

--- powerauth-push-server/src/test/resources/com/wultra/push/repository/model/PushDeviceRegistrationEntityTest.sql
+++ powerauth-push-server/src/test/resources/com/wultra/push/repository/model/PushDeviceRegistrationEntityTest.sql
@@ -4,4 +4,4 @@ insert into push_app_credentials(id, app_id) values
 insert into push_device_registration (id, platform, push_token, timestamp_last_registered, app_id) values
     (1, 'ios', 'token1', now(), 1);
 
-ALTER SEQUENCE push_device_registration_seq RESTART WITH 3;
+ALTER SEQUENCE push_device_registration_seq RESTART WITH 2;
